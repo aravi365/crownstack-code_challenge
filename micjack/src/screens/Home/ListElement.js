2838c3f0c9d1d9608d453f508b623096e6c921e2
Aravind <aravind.nair2010@gmail.com>
2021-07-01 12:45:45 +0530

    add checks for ticker

--- micjack/src/screens/Home/ListElement.js
+++ micjack/src/screens/Home/ListElement.js
@@ -11,16 +11,22 @@ export default function ListElement(props) {
         style={styles.avatarImage}
       />
       <View style={styles.innerView}>
-        <TextTicker
-          style={styles.tickerText}
-          duration={5000}
-          loop
-          bounce
-          repeatSpacer={50}
-          marqueeDelay={1000}>
-          {props.data.trackName || 'No title'}
-        </TextTicker>
-        <Text style={styles.collectionName} numberOfLines={2}>
+        {props.data?.trackName?.length > 40 ? (
+          <TextTicker
+            style={styles.tickerText}
+            duration={5000}
+            loop
+            bounce
+            repeatSpacer={50}
+            marqueeDelay={1000}>
+            {props.data.trackName || 'No title'}
+          </TextTicker>
+        ) : (
+          <Text style={styles.tickerText}>
+            {props.data.trackName || 'No title'}
+          </Text>
+        )}
+        <Text style={styles.collectionName} numberOfLines={3}>
           {props.data.collectionName}
         </Text>
         <View>
@@ -67,13 +73,22 @@ const styles = StyleSheet.create({
   },
   avatarImage: {width: 80, height: 80, borderRadius: 5},
   innerView: {marginLeft: 10},
-  tickerText: {width: 280, fontWeight: '800'},
-  collectionName: {width: 280, fontWeight: '500', color: '#495052'},
+  tickerText: {
+    width: 280,
+    fontWeight: 'bold',
+    paddingRight: 10,
+  },
+  collectionName: {
+    width: 280,
+    fontWeight: 'bold',
+    paddingRight: 10,
+    color: '#495052',
+  },
   artistName: {
     width: 280,
     fontSize: 12,
     color: 'rgb(0,190,200)',
-    fontWeight: '800',
+    fontWeight: 'bold',
     lineHeight: 24,
   },
   genreName: {
